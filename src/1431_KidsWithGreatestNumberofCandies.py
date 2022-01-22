17d0b2c14c9a9069eaa918d800ea88ee80f79428
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-01-21 22:59:47 -0500

    pycharm

---
+++ src/1431_KidsWithGreatestNumberofCandies.py
@@ -0,0 +1,15 @@
+def finalVal():
+    candies = [2, 3, 5, 1, 3]
+    extraCandies = 3
+
+    maxInCandies = max(candies)
+    rest= [False]*(len(candies))
+    for i in range(len(candies)):
+        if((candies[i] + extraCandies) > maxInCandies):
+            rest[i]= True
+    return rest
+
+if __name__ == '__main__':
+    print(finalVal())
+
+    # https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
