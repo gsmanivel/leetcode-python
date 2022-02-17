998cbc359da4fa084f6b9f471aa89928ae3dd0ce
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-02-16 22:26:26 -0500

    Feb 16

--- src/learn-python/typecast.py
+++ src/learn-python/typecast.py
@@ -16,6 +16,32 @@ def coding():
     print(type(mani_exp))
     print(type(int(mani_exp)))
 
+    # Invalid type conversion (Try it)
+    # sin2_exp = '12inbank'
+    # print(int(sin2_exp))
+
+    # Hilarious
+    print(type(12))
+    print(type(3.7))
+    print(type(True))
+    print(type('hi'))
+
+    print(int(12.0))
+    print(int('12'))
+    print(int(12.9))
+    print(int(12.999))
+    print(int(12.99999999999990899999999999))
+
+    print(float(12))
+    print(float('12'))
+
+    print(bool('True'))
+    print(bool('False'))  # Note  this
+
+    print(bool(0))    # Only bool(0) is considered as Real boolean - False
+    print(bool(1))
+    print(bool(10))
+
 
 if __name__ == '__main__':
     coding()
