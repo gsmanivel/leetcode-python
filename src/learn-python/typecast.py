d96810c40a96f092e1018fb1ba555a59be576106
gsmanivel@gmail.com <gsmanivel@gmail.com>
2022-02-16 22:00:25 -0500

    Feb 16

---
+++ src/learn-python/typecast.py
@@ -0,0 +1,21 @@
+def coding():
+    # variable declaration - 1
+    mani = sin2 = ayush = "manman-family"
+    print(mani)
+    print(sin2)
+    print(ayush)
+
+    # variable declaration - 2
+    mani_age, sin2_age, ayush_age = 34, 34, 6
+    print(mani_age)
+    print(sin2_age)
+    print(ayush_age)
+
+    # type conversion
+    mani_exp = '7'
+    print(type(mani_exp))
+    print(type(int(mani_exp)))
+
+
+if __name__ == '__main__':
+    coding()
